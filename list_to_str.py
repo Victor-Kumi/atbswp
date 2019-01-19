84fc651bb6480ef3a1ccbe4a2d5fb9baa5e5aed2
Victor-Kumi <46337485+Victor-Kumi@users.noreply.github.com>
2019-01-19 19:56:42 +0000

    Update list_to_str.py

--- list_to_str.py
+++ list_to_str.py
@@ -1,13 +1,18 @@
 def list_str(ls):
-    a=str(ls[0])+', '
-    i=0
-    for j in range(len(ls)-2):
-        i+=1
-        a=a+str(ls[i])+', '
-    if len(ls)==1:
-        return str(ls[0])
+    if len(ls)>0:
+        a=str(ls[0])+', '
+        i=0
+        for j in range(len(ls)-2): #this ensures that two items are left for ist and last cases.
+            i+=1
+            a=a+str(ls[i])+', '
+        if len(ls)==1:
+            return str(ls[0])
+        elif len(ls)>1:
+            return a+'and '+str(ls[-1])
     else:
-        return a+'and '+str(ls[-1])
-
+        return 'Your list is empty'
+    
+#An example
+#Change list value to test for various lengths of list.
 print ('\'',list_str([1,2,6 ]),'\'')
 
