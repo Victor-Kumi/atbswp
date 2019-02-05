ad62bbcbace7b618739989dac2934fb3f4a9916d
Victor-Kumi <46337485+Victor-Kumi@users.noreply.github.com>
2019-02-05 03:05:01 +0000

    Add files via upload

---
+++ searchRegexdir.py
@@ -0,0 +1,16 @@
+import re, os, sys
+
+#Accepts directory path as terminal argument.
+regexToken = input('Enter tokens to match:\n')
+for item in os.listdir(sys.argv[1]):
+    if re.compile(r'.txt').search(item): #same as 'if re.compile(r'.txt').search(item) != None:'
+        with open(item) as txtfile:
+            matchObject=re.compile(regexToken).findall(txtfile.read())
+            print('List contains result after matching regex tokens',regexToken,'in',item)
+            print(matchObject)
+            print('')
+            
+
+        
+
+
