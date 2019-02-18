3fb5919f6209ce2c6c9e4c87f764e09594c307bf
Victor-Kumi <46337485+Victor-Kumi@users.noreply.github.com>
2019-02-18 19:59:04 +0000

    Add files via upload

---
+++ chpt10_coinToss.py
@@ -0,0 +1,30 @@
+import random
+
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+
+def randomToss():
+    global toss
+    toss = random.randint(0, 1) # 0 is tails, 1 is heads
+
+    #convert 0 and 1 to tails and heads respectively.
+    if toss == 1:
+        toss = 'heads'
+    else:
+        toss = 'tails'
+
+toss = ''
+randomToss()
+
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    randomToss()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
